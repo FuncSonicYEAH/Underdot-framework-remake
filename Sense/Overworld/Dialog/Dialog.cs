a54435c3059f967b91f9241a335b42f7385f530d
FuncSonicYEAH <anvilstd123@outlook.com>
2026-01-11 20:46:48 +0800

    ennn

--- Sense/Overworld/Dialog/Dialog.cs
+++ Sense/Overworld/Dialog/Dialog.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 public partial class Dialog : CanvasLayer
 {
@@ -27,15 +28,27 @@ public partial class Dialog : CanvasLayer
 		DialogContainer.Show();
 	}
 
-	private void OnEnd()
+	private async void OnEnd()
 	{
-		Started = false;
 		DialogContainer.Hide();
+		await ToSignal(GetTree().CreateTimer(0.0025), "timeout");
+		Started = false;
 	}
 
 	public void DialogStart(string text)
 	{
-		Started = true;
-		Typer.StartTyping(text);
+		if (!Started)
+		{
+			Started = true;
+			Typer.StartTyping(text);
+		}
 	}
+
+	private static Task ToSignal(GodotObject source, string signal)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+        Callable callable = Callable.From(() => tcs.SetResult(true));
+        source.Connect(signal, callable);
+        return tcs.Task;
+    }
 }
